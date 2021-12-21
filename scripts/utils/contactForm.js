3605d269ad8dbbb5441257890a0ed6ed87bea0bb
Sylvie Melin <sy.melin@gmail.com>
2021-12-21 17:09:41 +0100

    Ajout de qq attr aria

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,19 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    modal.attr("aria-hidden", "false");
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    modal.attr("aria-hidden", "true");
 }
+
+
+// on récupère l'élément bouton de contact
+//const contactBtn = document.querySelector(".contact_button");
+// on écoute l'évènement "click" sur le bouton:  celui-ci déclenche la fermeture de la modale
+//contactBtn.addEventListener("click", displayModal);
+
+// Fermeture de la modale quand in appuie sur échap
