561e0aed8deb56fdf5776f8f67d63a1013ffc758
Sylvie Melin <sy.melin@gmail.com>
2022-01-12 00:00:06 +0100

    selectCopy css

--- scripts/utils/customSelect.js
+++ scripts/utils/customSelect.js
@@ -2,28 +2,28 @@ const sortSelect = document.getElementById("sort-select");
 const options = [].slice.call(sortSelect.children);
 
 const selectCopy = document.querySelector(".select-copy");
-const optionsCopy = [].slice.call(selectCopy.children);
-//console.log(optionsCopy);
+const optionsCopy = [].slice.call(selectCopy.children);;
 optionsCopy.forEach((option) => {
     option.addEventListener("click", function(e){
-        console.log(option + " a été cliquée");
+        //console.log(option + " a été cliquée");
         e.preventDefault();
         
-        let optionIndex = optionsCopy.indexOf(option);
-            //console.log("optionIndex", optionIndex);
-        let optionSelected = options[optionIndex];
-        let optionsNotSelected = optionsCopy.splice(optionIndex);
-        console.log("NOT SELECTED", optionsNotSelected);
+        option.setAttribute("selected", true);
+        console.log("au clic, on attribut option cliquée", option.getAttribute("selected"));
 
+        let optionIndex = optionsCopy.indexOf(option);
+        let optionSelected = options[optionIndex];
         if (selectCopy.classList.contains("select-open")){
 
             optionsCopy.forEach((option) => {
-               // option.classList.remove("option-selected");
-               // option.classList.add("option-notSelected");
-               // console.log(option.className);
+                console.log("Attribut selected", option.getAttribute("selected"));
+                if ((option.getAttribute("selected")) == "false"){
+                    option.classList.remove("option-selected");
+                    option.classList.add("option-notSelected");
+                    console.log("AHHHHHHH", option.className);
+                };
             });
 
-            optionsCopy[optionIndex].classList.remove("option-notSelected");
             console.log(optionsCopy[optionIndex].className);
 
             //on ferme les options du select
@@ -55,8 +55,29 @@ optionsCopy.forEach((option) => {
 
 const expandBtn = document.querySelector(".expandBtn");
 expandBtn.addEventListener("click", function(e){
+    e.preventDefault();
     console.log("HELLOOO");
+
     expandBtn.classList.toggle("expandBtn-less");
+    selectCopy.setAttribute("selected", true);
     selectCopy.classList.toggle("select-open");
     selectCopy.classList.toggle("select-close");
-})
+
+    optionsCopy.forEach((option) => {
+        option.setAttribute("selected", false);
+        //console.log(option.getAttribute("selected"));
+        option.style.cursor = "pointer";
+        if (option.classList.contains("option-notSelected")) {
+            option.classList.remove("option-notSelected");
+            option.classList.add("option-selected");
+        };
+        //console.log("Etat à l'ouverture des choix : ", option.className);
+    });
+
+
+
+
+
+
+    
+});
