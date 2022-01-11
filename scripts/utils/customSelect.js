1cf64db7e3b9476fe4fb98c5a6b236d0e42cbd6e
Sylvie Melin <sy.melin@gmail.com>
2022-01-11 18:48:50 +0100

    css pour customSelect

--- scripts/utils/customSelect.js
+++ scripts/utils/customSelect.js
@@ -7,24 +7,56 @@ const optionsCopy = [].slice.call(selectCopy.children);
 optionsCopy.forEach((option) => {
     option.addEventListener("click", function(e){
         console.log(option + " a été cliquée");
-       // option.parentElement.style.width = "150px";
+        e.preventDefault();
+        
         let optionIndex = optionsCopy.indexOf(option);
-        //console.log("optionIndex", optionIndex);
+            //console.log("optionIndex", optionIndex);
         let optionSelected = options[optionIndex];
-       // console.log(optionSelected);
-        let optionSelectedValue = optionSelected.getAttribute("value")
-        //console.log(optionSelectedValue);
-        sortSelect.value = optionSelectedValue;
+        let optionsNotSelected = optionsCopy.splice(optionIndex);
+        console.log("NOT SELECTED", optionsNotSelected);
 
-        //On vide le conteneur de cartes media
-        const mediaWrapper = document.querySelector(".mediaWrapper");
-        mediaWrapper.parentElement.removeChild(mediaWrapper);
+        if (selectCopy.classList.contains("select-open")){
 
-         //On crée un nouveau fltre en fonction de select.value et on réaffiche les cartes media
-        const filter = new FilterFactory(sortSelect.value, photographerMedia);
-        filter.createAFilter();
-        displayMedia(photographerMedia);
+            optionsCopy.forEach((option) => {
+               // option.classList.remove("option-selected");
+               // option.classList.add("option-notSelected");
+               // console.log(option.className);
+            });
+
+            optionsCopy[optionIndex].classList.remove("option-notSelected");
+            console.log(optionsCopy[optionIndex].className);
+
+            //on ferme les options du select
+            expandBtn.classList.toggle("expandBtn-less");
+            selectCopy.classList.toggle("select-open");
+            selectCopy.classList.toggle("select-close");
+
+            
+           
+        // console.log(optionSelected);
+            let optionSelectedValue = optionSelected.getAttribute("value")
+            //console.log(optionSelectedValue);
+            sortSelect.value = optionSelectedValue;
+
+            //On vide le conteneur de cartes media
+            const mediaWrapper = document.querySelector(".mediaWrapper");
+            mediaWrapper.parentElement.removeChild(mediaWrapper);
+
+            //On crée un nouveau fltre en fonction de select.value et on réaffiche les cartes media
+            const filter = new FilterFactory(sortSelect.value, photographerMedia);
+            filter.createAFilter();
+            displayMedia(photographerMedia);
+
+        };
 
     });
 
 });
+
+const expandBtn = document.querySelector(".expandBtn");
+expandBtn.addEventListener("click", function(e){
+    console.log("HELLOOO");
+    expandBtn.classList.toggle("expandBtn-less");
+    selectCopy.classList.toggle("select-open");
+    selectCopy.classList.toggle("select-close");
+})
