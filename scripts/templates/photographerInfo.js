9c8433cfd75743c759616217064295ddd6e11364
Sylvie Melin <sy.melin@gmail.com>
2022-01-21 17:52:56 +0100

    accessibilité info

--- scripts/templates/photographerInfo.js
+++ scripts/templates/photographerInfo.js
@@ -11,7 +11,14 @@ class PhotographerInfo {
         totalLikes.textContent = likeTotal;
 
         const price = document.getElementById("price");
+        price.setAttribute("aria-hidden", true);
         price.textContent = this._photographerData.price + "€ / jour";
+        const priceSpan = document.createElement('span');
+        priceSpan.classList.add("screenreader-text");
+        priceSpan.textContent = this._photographerData.price +"€ par jour";
+
+        info.appendChild(priceSpan);
+
     };
 
 };
