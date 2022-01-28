7c7f4088c953faff76d7c55c4747a42f255e1ae9
Sylvie Melin <sy.melin@gmail.com>
2022-01-28 16:47:23 +0100

    accessibilité tri

--- scripts/templates/customSelect.js
+++ scripts/templates/customSelect.js
@@ -10,75 +10,102 @@ class CustomSelect {
 
 }
 
-
 const sortSelect = document.getElementById("sort-select");
 const options = [].slice.call(sortSelect.children);
 
 const selectCopy = document.querySelector(".select-copy");
-const optionsCopy = [].slice.call(selectCopy.children);;
+const optionsCopy = [].slice.call(selectCopy.children);
+
+const expandBtn = document.querySelector(".expandBtn");
+
+
+
+//////////// Fonction utile pour l'évenement sur une option de la listbox /////////////////
+
+function selectAnOption (option) {
+
+    optionsCopy.forEach((childOption) => {
+        childOption.setAttribute("aria-selected", false);
+    });
+    
+    selectCopy.setAttribute("aria-expanded", "false");
+    selectCopy.focus();
+    
+    //Au clic, on attribut option cliquée", option.getAttribute("aria-selected"));
+    option.setAttribute("aria-selected", true);
+    
+    //On récupère l'id de l'option cliquée
+    let optionSelected = option.getAttribute("id");
+
+    if (selectCopy.classList.contains("select-open")){
+
+        optionsCopy.forEach((childOption) => {
+            //console.log("Attribut aria-selected", option.getAttribute("aria-selected"));
+            if ((childOption.getAttribute("aria-selected")) == "false"){
+                childOption.classList.remove("option-selected");
+                childOption.classList.add("option-notSelected");
+            };
+        });
+
+        //on ferme les options du select
+        expandBtn.setAttribute("aria-expanded", "false");
+        expandBtn.classList.toggle("expandBtn-less");
+        selectCopy.classList.toggle("select-open");
+        selectCopy.classList.toggle("select-close");
+
+
+        //On vide le conteneur de cartes media
+        const mediaWrapper = document.querySelector(".mediaWrapper");
+        mediaWrapper.parentElement.removeChild(mediaWrapper);
+
+        //On crée un nouveau filtre en fonction de select.value et on réaffiche les cartes media
+      
+        sortSelect.value = optionSelected
+        const filter = new FilterFactory(sortSelect.value, photographerMedia);
+        filter.createAFilter();
+        displayMedia(photographerMedia);
+
+    };
+};
+
+
+//////////// Evenement sur une option de la listbox /////////////////
+
+//au  clic sur une option
 optionsCopy.forEach((option) => {
     option.addEventListener("click", function(e){
         //console.log(option + " a été cliquée");
         e.preventDefault();
-        
-        optionsCopy.forEach((option) => {
-            option.setAttribute("selected", false);
-        });
-        
-        selectCopy.setAttribute("selected", "false");
-        
-        //Au clic, on attribut option cliquée", option.getAttribute("selected"));
-        option.setAttribute("selected", true);
-        
-        //On récupère l'id de l'option cliquée
-        let optionSelected = option.getAttribute("id");
-
-        if (selectCopy.classList.contains("select-open")){
-
-            optionsCopy.forEach((option) => {
-                //console.log("Attribut selected", option.getAttribute("selected"));
-                if ((option.getAttribute("selected")) == "false"){
-                    option.classList.remove("option-selected");
-                    option.classList.add("option-notSelected");
-                };
-            });
-
-            //on ferme les options du select
-            expandBtn.classList.toggle("expandBtn-less");
-            selectCopy.classList.toggle("select-open");
-            selectCopy.classList.toggle("select-close");
-
-
-            //On vide le conteneur de cartes media
-            const mediaWrapper = document.querySelector(".mediaWrapper");
-            mediaWrapper.parentElement.removeChild(mediaWrapper);
-
-            //On crée un nouveau filtre en fonction de select.value et on réaffiche les cartes media
-          
-            sortSelect.value = optionSelected
-            const filter = new FilterFactory(sortSelect.value, photographerMedia);
-            filter.createAFilter();
-            displayMedia(photographerMedia);
-
-        };
-
+        selectAnOption(option);
     });
-
 });
 
-//Evenement sur le bouton expand du select
+//avec ENTER sur une option
+optionsCopy.forEach((option) => {
+    option.addEventListener("keyup", function(e){
+        e.preventDefault();
+        if (e.key === "Enter") {
+            selectAnOption(option);
+        };
+    });
+});
 
-const expandBtn = document.querySelector(".expandBtn");
-expandBtn.addEventListener("click", function(e){
-    e.preventDefault();
 
-    let state = selectCopy.getAttribute("selected");
+
+//////////// Fonction utile pour !'évenement sur le bouton expand du select /////////////////
+
+function changeSelectDisplay () {
+
+    let state = selectCopy.getAttribute("aria-expanded");
     console.log(state);
 
     //on affiche le menu select
+    expandBtn.setAttribute("aria-expanded", "true");
     expandBtn.classList.toggle("expandBtn-less");
     selectCopy.classList.toggle("select-open");
     selectCopy.classList.toggle("select-close");
+
+    selectCopy.focus();
 
     optionsCopy.forEach((option) => {
         option.style.cursor = "pointer";
@@ -92,18 +119,36 @@ expandBtn.addEventListener("click", function(e){
     //Selon l'état du select enregistré au clic
     if (state == "false") {
         console.log("on vient de boucler");
-        selectCopy.setAttribute("selected", "true");
-        console.log(selectCopy.getAttribute("selected"));
+        selectCopy.setAttribute("aria-expanded", "true");
+        console.log(selectCopy.getAttribute("aria-expanded"));
     } else {
         console.log("on vient d'ouvrir et on veut refermer")
-        selectCopy.setAttribute("selected", "false");
-        console.log(selectCopy.getAttribute("selected"));
+        expandBtn.setAttribute("aria-expanded", "false");
+        selectCopy.setAttribute("aria-expanded", "false");
+        console.log(selectCopy.getAttribute("aria-expanded"));
         optionsCopy.forEach((option)=> {
 
-            if ((option.getAttribute("selected")) !== "true") {
+            if ((option.getAttribute("aria-selected")) !== "true") {
                 option.classList.remove("option-selected");
                 option.classList.add("option-notSelected");
             };
         })   
-    }
+    };
+};
+
+
+//////////// Evenement sur le bouton expand du select /////////////////
+
+//au  clic sur le bouton expand
+expandBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    changeSelectDisplay();
+});
+
+ //avec ENTRER sur le bouton expand
+ expandBtn.addEventListener("keyup", function(e){
+    e.preventDefault();
+    if (e.key === "Enter") {
+        changeSelectDisplay;
+    };
 });
